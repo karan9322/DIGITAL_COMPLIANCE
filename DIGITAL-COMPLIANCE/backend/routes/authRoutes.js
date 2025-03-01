1d91833122d5b7dbc4bdf1248b2434999997e2e1
karan telgad <karan123@gmail.com>
2025-03-02 02:27:41 +0530

    Added test route to check authRoutes

--- DIGITAL-COMPLIANCE/backend/routes/authRoutes.js
+++ DIGITAL-COMPLIANCE/backend/routes/authRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { login, register } = require("../controllers/authController");
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.json({ message: "Auth routes are working!" });
+});
+
 // Login route
 router.post("/login", login);
 
